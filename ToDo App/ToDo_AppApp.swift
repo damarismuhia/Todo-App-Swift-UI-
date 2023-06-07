f47c2e545bdfecae8fee77101012a819d3f20541
damarismuhia <muhiadamaris12@gmail.com>
2023-06-07 22:06:28 +0300

    Initial Commit

---
+++ ToDo App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo App
+//
+//  Created by Damaris Muhia on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
